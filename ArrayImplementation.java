919ecbd6d11fedf1a989611eae425d339b99e11e
Amal-07x <amalalawadhi005@gmail.com>
2026-07-22 21:11:11 +0300

    Creat Task class
    Added Task class with attributes and methods to represent task information.

--- ArrayImplementation.java
+++ ArrayImplementation.java
@@ -1,1 +1,47 @@
 
+public class Task {
+
+    private int taskId;
+    private String taskName;
+    private int priority;
+    private int arrivalOrder;
+
+
+    public Task(int taskId, String taskName, int priority, int arrivalOrder) {
+
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.priority = priority;
+        this.arrivalOrder = arrivalOrder;
+
+    }
+
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+
+    public int getPriority() {
+        return priority;
+    }
+
+
+    public int getArrivalOrder() {
+        return arrivalOrder;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "Task ID: " + taskId +
+                ", Name: " + taskName +
+                ", Priority: " + priority +
+                ", Arrival Order: " + arrivalOrder;
+
+    }
+
+}
+
+
