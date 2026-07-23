088262d2fec16748ce664dc77b205f2c62958b3c
Juri838 <Jory.m.bawazir@gmail.com>
2026-07-23 19:08:50 +0300

    Add LinkedListQueue and Task classes
    Implemented a linked list queue with enqueue functionality.

--- LinkedListQueue.java
+++ LinkedListQueue.java
@@ -1,1 +1,43 @@
+//class to define linked task
+class Task{
+  int taskId;
+  String taskName;
+  Task next;
 
+public Task(int taskId,String taskName){
+  this.taskId=taskId;
+  this.taskName=taskName;
+  this.next=null;
+}
+
+//override
+public String toString(){
+  return "Task[ID="+ taskId +",Name=" +taskName +"]";
+}
+}
+
+//Linked List Queue Implementation
+class LinkedListQueue{
+  private Task front;
+  private Task rear;
+  private int size;
+//Zero argument constructor
+  public LinkedListQueue(){
+    this.front=this.rear=null;
+    this.size=0;
+  }
+//add tasks at the end of the queue
+public void enqueue(int id,String name){
+  Task newNode=newTask(id,Name):
+    if(this.rear==null){
+    this.front=this.rear=newNode;
+  }else{
+    this.rear.next=newNode;
+    this.rear=newNode;//update rear
+  }
+  size++;
+  System.out.println("Task Enqueued:"+newNode);
+}
+  
+}
+  
